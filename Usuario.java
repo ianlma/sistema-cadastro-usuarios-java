b43d02bd93579a6d5770c3f34049af933baea94f
Ian Lima <ianlimadealmeida3@gmail.com>
2026-03-14 22:35:52 -0300

    Refactor Usuario class and remove SistemaCadastro

---
+++ Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+
+    String nome;
+    String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+}
+
